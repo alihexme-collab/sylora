e88295f0455d5b38f3df33d7e2cd5418c5fed571
Ali <alihexme@gmail.com>
2026-07-05 13:20:51 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from receive import *
 
 def main():
-    print("=== BUILD VERSION: 2026-07-05-v1 ===")
+    print("=== BUILD VERSION: 2026-07-05-v2 ===")
 
     print("در حال شروع...")
     app.run_polling()
 
 if __name__ == "__main__":
-    print(bus.listeners)
     main()
     

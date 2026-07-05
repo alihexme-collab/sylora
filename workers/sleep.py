3d73b005281120110fb9dd168e71b5842686c9f7
Ali <alihexme@gmail.com>
2026-07-05 13:04:56 +0330

    Update sleep.py

--- workers/sleep.py
+++ workers/sleep.py
@@ -2,6 +2,7 @@ from sqlalchemy import select
 from database.db_manager import get_db
 from database.model import *
 from .bus import bus
+from sqlalchemy.ext.asyncio import AsyncSession
 
 class Sleep:
     async def get(self, **data):
@@ -9,6 +10,34 @@ class Sleep:
         self.message = data.get("message")
         async with get_db() as self.session:
             await self._sleep()
+    async def tick(self, **data):
+         async with get_db() as session:
+              session: AsyncSession
+              query = select(
+                   Character,
+                   CharacterStats,
+              ).where(
+                   Character.is_alive == True and Character.character_id == CharacterStats.character_id
+              )
+              results = await session.execute(query)
+              for result in results.scalars().all():
+                    if result.hp + 10 > result.base_hp:
+                        result.hp = result.base_hp
+                    else:
+                        result.hp += 10
+
+                    if result.mana + 1 > result.base_mana:
+                        result.mana = result.base_mana
+                    else:
+                        result.mana += 1
+
+                    if result.energy + 5 > result.base_energy:
+                        result.energy = result.base_energy
+                    else:
+                        result.energy += 5
+                    await session.commit()
+                        
+                   
 
     async def _sleep(self):
         await self.get_character()
@@ -72,3 +101,4 @@ class Sleep:
 move = Sleep()
 
 bus.listen("SLEEP", move.get)
+bus.listen("TICK", move.tick)

3df9a4ff0e14835db7a21dada428ee68d291b019
Ali <alihexme@gmail.com>
2026-07-06 21:27:23 +0330

    Update send.py

--- workers/send.py
+++ workers/send.py
@@ -21,7 +21,6 @@ class Sender:
                 text=text,
                 reply_markup=reply_markup,
                 parse_mode=parse_mode,
-                **kwargs
             )
 
         if chat_id is None:
@@ -33,7 +32,6 @@ class Sender:
             text=text,
             reply_markup=reply_markup,
             parse_mode=parse_mode,
-            **kwargs
         )
         return sent
 
